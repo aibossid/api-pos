e71da19445c04ad6178c4cb2bf4f6e352d575b4a
Aibossid <alifpratama.contact@gmail.com>
2025-08-20 15:23:44 +0700

    update app.js add new routes

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ const express = require("express");
 require("dotenv").config();
 
 const productRoutes = require("./routes/productRoutes");
+const cartRoutes = require("./routes/cartRoutes");
+const checkoutRoutes = require("./routes/checkoutRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
+app.use("/api/report", checkoutRoutes);
 
 app.get("/", (req, res) => {
   res.status(200).send(`<h1>Welcome to database </h1>`);
